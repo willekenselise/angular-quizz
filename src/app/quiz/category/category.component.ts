8cfccda0b490ac33cc0c581fafabc6fc94f149c2
willekenselise <willekens.elise@orange.fr>
2023-10-17 12:19:04 +0200

    add filter feature

--- src/app/quiz/category/category.component.ts
+++ src/app/quiz/category/category.component.ts
@@ -1,20 +1,21 @@
 import { Component } from '@angular/core';
 import { CategoryService } from 'src/app/shared/services/category.service';
-import { FormBuilder, FormGroup } from "@angular/forms";
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.scss']
+  styleUrls: ['./category.component.scss'],
 })
 export class CategoryComponent {
   categories: any[] = this.categoryService.categories;
+  filteredCategories: any[] = this.categoryService.categories;
   searchForm!: FormGroup;
 
   constructor(
     private categoryService: CategoryService,
     private fb: FormBuilder
-     ) { }
+  ) {}
 
   ngOnInit(): void {
     this.categoryService.getCategories();
@@ -22,10 +23,24 @@ export class CategoryComponent {
       search: [''],
     });
   }
+  search(): void {
+    const { search } = this.searchForm.value;
+    if (search) {
+      this.filteredCategories = this.categories.filter((c) =>
+        this.normalizeString(c.name).includes(this.normalizeString(search))
+      );
+    }
+  }
+  
+  normalizeString(input: string): string {
+    return input
+      .normalize("NFD") 
+      .replace(/[\u0300-\u036f]/g, "") 
+      .toLowerCase();
+  } 
 
-  search (){
-    console.log("search")
-   }
+  reset(): void {
+    console.log(this.categories);
+   this.filteredCategories = this.categories;
+  }
 }
-
-
--- src/app/shared/services/category.service.ts
+++ src/app/shared/services/category.service.ts
@@ -17,8 +17,4 @@ export class CategoryService {
     });
   }
 
-
-  searchCategory(){
-  }
-
 }
